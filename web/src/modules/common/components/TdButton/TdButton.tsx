93dcc0e15ffed8c0221de019a774e1e6c2bfe6fa
Dhiraj Arun <dhrjarun@gmail.com>
2023-03-03 14:08:49 +0530

    refactor(web): refactor TdButton component

--- web/src/modules/common/components/TdButton/TdButton.tsx
+++ web/src/modules/common/components/TdButton/TdButton.tsx
@@ -9,8 +9,9 @@ export function TdButton(props: TdButtonProps) {
   return (
     <button
       className={clsx(
-        className,
-        'text-xiv text-gray-500 font-medium px-2.5 py-2 rounded-lg shadow-[0_5px_0_0px] shadow-gray-200 border-2 border-gray-200 hover:shadow-gray-300 hover:border-gray-300 active:shadow-[inset_0_0] active:translate-y-[5px]'
+        'inline-flex h-[30px] items-center px-3 border border-gray-200 rounded-lg text-xiv text-gray-500 font-medium space-x-1.5',
+        'shadow-[0_3px_0_0px] shadow-gray-200  hover:shadow-gray-300 hover:border-gray-300 active:shadow-[inset_0_0] active:translate-y-[3px]',
+        className
       )}
       {...rest}
     >
