e26ac5fbe9808e2ea647dd45e1989fff0f778d3b
Dhiraj Arun <dhrjarun@gmail.com>
2023-03-07 21:36:53 +0530

    feat(db): add db package

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -3,7 +3,9 @@
  * @link https://www.prisma.io/docs/support/help-articles/nextjs-prisma-client-dev-practices
  */
 import { PrismaClient } from '@prisma/client';
-import { env } from '../env';
+import { env } from './env';
+
+export * from '@prisma/client';
 
 const prismaGlobal = global as typeof global & {
   prisma?: PrismaClient;
